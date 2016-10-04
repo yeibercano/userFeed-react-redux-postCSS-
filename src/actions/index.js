5b9623f2ce440bf34c803f47e64716abb33436a2
Yeiber Cano <yeibercano@gmail.com>
2016-10-04 03:27:49 -0700

    cleanup console

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,11 +3,9 @@ import { browserHistory } from 'react-router';
 import * as types from './types';
 
 export const getFeed = () => {
-	console.log('get feed called')
 	return dispatch => {
     axios.get('/feed')
     .then( response => { 
-    	console.log('response', response.data.data.recentActivities)
     	dispatch({ type: types.GET_FEED, payload: response.data.data.recentActivities })
     })
     .catch(err => {
